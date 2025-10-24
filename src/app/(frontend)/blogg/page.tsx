614f448b54a217b837aef6c4332828356bd0029e
latif-PC <latif_1995@live.no>
2025-10-24 02:31:57 +0200

    brukte REST API & fetch

--- src/app/(frontend)/blogg/page.tsx
+++ src/app/(frontend)/blogg/page.tsx
@@ -1,31 +1,35 @@
+import Link from 'next/link'
 import React from 'react'
 
-import { getPayload } from 'payload'
-import config from '@payload-config'
-
-import Link from 'next/link'
-
 async function BlogListPage() {
-  const payload = await getPayload({ config })
+  let posts = []
 
-  const posts = await payload.find({
-    collection: 'posts',
-  })
+  try {
+    // Using Payload's REST API endpoint format
+    const req = await fetch('http://localhost:3000/api/posts?')
 
-  console.log('Posts:', posts)
+    if (!req.ok) {
+      throw new Error(`HTTP error! status: ${req.status}`)
+    }
 
+    const data = await req.json()
+
+    posts = data.docs || []
+  } catch (err) {
+    console.log('Error fetching posts:', err)
+  }
+
+  console.log('Fetched posts:', posts)
   return (
     <main>
       <h1>Blog Posts</h1>
 
       <ul>
-        {/* <pre>{JSON.stringify(posts, null, 2)}</pre> */}
-
-        {posts.docs.map((post) => (
-          <li key={post.slug}>
-            {' '}
-            <Link href={`/blogg/${post.slug}`}>{post.title}</Link>{' '}
-          </li>
+        {' '}
+        {posts.map((post: { id: string; slug: string }) => (
+          <Link key={post.id} href={`/blogg/${post.slug}`}>
+            <li>{post.slug}</li>
+          </Link>
         ))}
       </ul>
     </main>
